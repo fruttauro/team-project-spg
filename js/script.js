14f9a76a200a815f1fedf7a3ada3ee5416ea8c51
Daniel Fruttauro <fruttauro@gmail.com>
2015-12-28 17:18:44 +0000

    implemented js that loads div content on back/forward in the browser

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+// Wonderfully hand crafted hack to load div's on use of back/forward buttons in the browser
+var popStateFired = false;
+window.onpopstate = function(event) {
+	if (popStateFired) {
+		return;
+	}
+
+	popStateFired = true
+	window.location = document.location
+	popStateFired = false
+};
+
+// Dynamic loading of questions and answers
 var questionObjects = [];
 
 function question(questionNo, question, positiveAnswer, negativeAnswer) {

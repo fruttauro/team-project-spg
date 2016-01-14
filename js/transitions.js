0f56d7ce52ba4469c483ed49493874eaf29c8324
Daniel Fruttauro <fruttauro@gmail.com>
2016-01-14 12:39:29 +0000

    Get hold of old and new div in preperation for adding random animations on navigation

--- js/transitions.js
+++ js/transitions.js
@@ -1,5 +1,12 @@
 $(".questionSelect > a, .btn").click(function(){
-	if ("onhashchange" in window) {
+	
+	window.onhashchange = function(e){
+		
+		var oldDiv = "#" + e.oldURL.split("#")[1];
+		var newDiv = "#" + e.newURL.split("#")[1];
+		console.log("new Div " + newDiv)
+		console.log("old Div " + oldDiv)
+
 		$("body").hide()
 		$("body").fadeIn(500);
 	}
